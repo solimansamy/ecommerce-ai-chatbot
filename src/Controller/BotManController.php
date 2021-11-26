2e8de1b5ca93ce29a6f0b5ca68e2a3124f83631f
Soliman Samy <soliman1samy@gmail.com>
2021-11-26 13:48:24 +0200

    Install messenger webhook

--- src/Controller/BotManController.php
+++ src/Controller/BotManController.php
@@ -16,15 +16,31 @@ use App\Services\DialogFlowReceivedMiddleware;
 class BotManController extends Controller
 {
     const config = [
-            'facebook' => [
-                'token' => 'EAAGWjfdH1esBAHDLeCCKep3m3lNDdgZApyiOQv3T0kUZBl5FKMpszKSyGwaXekMRl4nMG9UZAqsIZB3eKjvPZC2CjZARAEZATBjLVpAHcX0LUCb2qRyUZB76NLYBuTn4K7a5h4XhNkLIo567s57Sz3Debh2aacdaLALbZAMTeLh0X7TAzHy3x4VqqS2C2O6kl3IgZD',
-                'app_secret' => '177e060025caac02951004d19dab37d1',
-                'verification'=>'YESCOMIN',
-            ]
-        ];
+        'facebook' => [
+            'token' => 'EAAGWjfdH1esBAHDLeCCKep3m3lNDdgZApyiOQv3T0kUZBl5FKMpszKSyGwaXekMRl4nMG9UZAqsIZB3eKjvPZC2CjZARAEZATBjLVpAHcX0LUCb2qRyUZB76NLYBuTn4K7a5h4XhNkLIo567s57Sz3Debh2aacdaLALbZAMTeLh0X7TAzHy3x4VqqS2C2O6kl3IgZD',
+            'app_secret' => '177e060025caac02951004d19dab37d1',
+            'verification' => 'YESCOMIN',
+        ]
+    ];
 
-    public function botmanWebhook(Request $request)
+    const VERIFY_TOKEN = 'riseup';
+
+    public function botManWebHook(Request $request)
     {
+        // Edit Callback URL
+        if($request->isMethod('GET')) {
+            return $this->verifyWebHook($request);
+        }
+
+        // Messenger is talking
+        if($request->isMethod('POST')) {
+            return new Response();
+        }
+
+        if($request->query->get('hub.mode') == 'subscribe' && $request->query->get('hub.verify_token') == self::VERIFY_TOKEN) {
+            return new Response('subscribed', 200);
+        }
+
         DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
         $botman = BotManFactory::create($this::config);
 //        $middleware = new DialogFlowReceivedMiddleware();
@@ -43,6 +59,15 @@ class BotManController extends Controller
         $botman->listen();
 
         return new Response();
+    }
+
+    private function verifyWebHook(Request $request)
+    {
+        if($request->query->get('hub_mode') == 'subscribe' && $request->query->get('hub_verify_token') == self::VERIFY_TOKEN) {
+            return new Response($request->query->get('hub_challenge'), 200);
+        }
+
+        return new Response('Not Found', 404);
     }
 
     public function testDialogFlow(Request $request)
