4a22711eddc65eb9adc435300ae94d2aa7f875a2
Soliman Samy <soliman1samy@gmail.com>
2021-11-26 09:47:50 +0200

    Adding some annotations to variables and methods

--- src/Services/dialogFlowAgent.php
+++ src/Services/dialogFlowAgent.php
@@ -1,24 +1,35 @@
 <?php
 
-
 namespace App\Services;
 
-use Exception;
 use Google\Cloud\Dialogflow\V2\AudioEncoding;
 use Google\Cloud\Dialogflow\V2\InputAudioConfig;
 use Google\Cloud\Dialogflow\V2\QueryInput;
 use Google\Cloud\Dialogflow\V2\SessionsClient;
 use Google\Cloud\Dialogflow\V2\TextInput;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Google\Cloud\Dialogflow\V2\QueryResult;
 
 class dialogFlowAgent
 {
+    /** @var SessionsClient */
     private $sessionsClient;
+
+    /** @var string */
     private $sessionName ;
+
+    /** @var string */
     private $projectId ;
+    
+    /** @var string */
     private $credentialPath;
+
+    /** @var string */
     protected $sessionId ;
+
+    /** @var string */
     protected $language;
+
+    /** @var QueryResult */
     protected $dialogFlowResponse;
 
     public function setupConfiguration($businessPurb)
@@ -28,14 +39,25 @@ class dialogFlowAgent
 //        $this->credentialPath = getenv('DIALOGFLOW_PATH_'.$businessPurb);
         return $this;
     }
+
+    /**
+     * @return string
+     */
     public function getSessionId()
     {
         return $this->sessionId ;
     }
-    public function openSession($sessionId=false)
+
+    /**
+     * @param bool $sessionId
+     * @return $this
+     * @throws \Google\ApiCore\ValidationException
+     */
+    public function openSession($sessionId = false)
     {
-        if($sessionId)
-            $this->sessionId  = $sessionId ;
+        if($sessionId) {
+            $this->sessionId  = $sessionId;
+        }
         $credential = array('credentials' => '/home/soliman/Projects/Riseup/woocommerce-chatbot-v9sf-5ffd1ecea7c5.json');
         $this->sessionsClient = new SessionsClient($credential);
         $this->sessionName = $this->sessionsClient->sessionName('woocommerce-chatbot-v9sf', uniqid());
@@ -47,6 +69,13 @@ class dialogFlowAgent
         $this->sessionsClient->close();
     }
 
+    /**
+     * @param $input
+     * @param $inputType
+     * @param string $languageCode
+     * @return QueryResult
+     * @throws \Google\ApiCore\ApiException
+     */
     public function detectIntent($input, $inputType, $languageCode='en-US')
     {
         if ($inputType == 'audio') {
@@ -55,9 +84,14 @@ class dialogFlowAgent
         else {
             return $this->detectIntentText($input, $languageCode);
         }
-
     }
 
+    /**
+     * @param $text
+     * @param $languageCode
+     * @return QueryResult
+     * @throws \Google\ApiCore\ApiException
+     */
     protected function detectIntentText($text, $languageCode)
     {
         $textInput = new TextInput();
@@ -70,6 +104,12 @@ class dialogFlowAgent
         return  $response->getQueryResult();
     }
 
+    /**
+     * @param $audioFilePath
+     * @param $languageCode
+     * @return QueryResult
+     * @throws \Google\ApiCore\ApiException
+     */
     protected function detectAudioIntent($audioFilePath, $languageCode)
     {
         $flacFile = $this->container->get(AudioMessageAdapter::class)->convert($audioFilePath, 'flac');
@@ -92,6 +132,7 @@ class dialogFlowAgent
         $content->setFulfilment($this->dialogFlowResponse->getFulfillmentText());
         return $content ;
     }
+
     public function FallbackIntent()
     {
         $content = new EntityModel();
@@ -100,6 +141,7 @@ class dialogFlowAgent
         $content->setFulfilment($this->dialogFlowResponse->getFulfillmentText());
         return $content ;
     }
+
     protected function ExtractParams()
     {
         $params = array();
@@ -111,6 +153,7 @@ class dialogFlowAgent
         }
         return $params ;
     }
+
     protected function detectedContent()
     {
         $content = new EntityModel();
